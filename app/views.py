493043701449b9710f0aa19ec19324b036431b0f
Maxim Kocheshkov <gror_godfroy@mail.ru>
2020-07-27 20:13:42 +0300

    версия 2

--- app/views.py
+++ app/views.py
@@ -1,4 +1,6 @@
 from collections import Counter
+
+from django.http import HttpResponse
 from django.shortcuts import render_to_response
 
 # в реальных проектах так не стоит делать
@@ -10,28 +12,31 @@ counter_click = Counter()
 def index(request):
     if 'test' in request.GET.get('from-landing'):
         counter_click['test'] += 1
-        counter_show['index'] += 1
         return render_to_response('index.html')
     else:
         counter_click['original'] += 1
-        counter_show['index'] += 1
         return render_to_response('index.html')
 
 
 def landing(request):
     if 'original' in request.GET.get('ab-test-arg'):
         landing_page = render_to_response('landing.html')
+        counter_show['index'] += 1
         return landing_page
     elif 'test' in request.GET.get('ab-test-arg'):
         landing_page_test = render_to_response('landing_alternate.html')
+        counter_show['index'] += 1
         return landing_page_test
 
 
 def stats(request):
-    test_conversion = counter_show['index']/counter_click['test']
-    original_conversion = counter_show['index']/counter_click['original']
-    context = {
-        'test_conversion': test_conversion,
-        'original_conversion': original_conversion,
-    }
-    return render_to_response('stats.html', context)
+    try:
+        test_conversion = counter_show['index']/counter_click['test']
+        original_conversion = counter_show['index']/counter_click['original']
+        return render_to_response('stats.html', context={
+            'test_conversion': test_conversion,
+            'original_conversion': original_conversion,
+        })
+    except ZeroDivisionError:
+        msg = 'Невозможно произвести расчет статистических данных'
+        return HttpResponse(msg)
